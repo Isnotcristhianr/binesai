1e269711ba04bdc0852cdd093d150f4824fd6652
Cristhian David Recalde <crecalde@neaternotes.com>
2024-11-17 19:16:45 -0500

    style: enhance flipcard component with improved shadows, text styling, and transition effects

--- src/components/main/intro/widgets/flipcard.vue
+++ src/components/main/intro/widgets/flipcard.vue
@@ -11,20 +11,20 @@ const props = defineProps<{
   <div class="flip-card w-96 h-96 perspective">
     <div class="flip-card-inner">
       <!-- Front Side -->
-      <div class="flip-card-front card bg-base-100 image-full shadow-xl">
+      <div class="flip-card-front card bg-base-100 image-full shadow-lg hover:shadow-2xl transition-shadow duration-500 ease-in-out">
         <figure>
-          <img :src="image" />
+          <img :src="image" class="w-full h-full object-cover" />
         </figure>
         <div class="card-body">
-          <h2 class="card-title flex items-center justify-center h-full text-2xl">
+          <h2 class="card-title flex items-center justify-center h-full text-2xl font-semibold">
             {{ title }}
           </h2>
         </div>
       </div>
       <!-- Back Side -->
-      <div class="flip-card-back card bg-base-200 shadow-xl flex items-center justify-center">
+      <div class="flip-card-back card bg-base-200 shadow-lg flex items-center justify-center text-white transition-colors duration-500 ease-in-out">
         <div class="card-body h-full flex items-center justify-center">
-          <p class="text-2xl text-center">
+          <p class="text-2xl text-center  p-4 text-secondary font-semibold">
             {{ description }}
           </p>
         </div>
@@ -35,7 +35,7 @@ const props = defineProps<{
 
 <style scoped>
 .perspective {
-  perspective: 1000px;
+  perspective: 1200px;
 }
 
 .flip-card-inner {
@@ -43,7 +43,7 @@ const props = defineProps<{
   width: 100%;
   height: 100%;
   text-align: center;
-  transition: transform 0.8s;
+  transition: transform 0.8s ease-in-out;
   transform-style: preserve-3d;
 }
 
